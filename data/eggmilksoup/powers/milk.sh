d34dc68169e8324c28dc1a8e235df7b90b10b9b5
Egg Milk Soup <egg@milksoup.net>
2022-06-25 13:01:18 -0500

    workaround cows, goats unable to be milked
    Due to the refactoring of eggmilksoup:egg, cows and goats can no longer be
    milked. Hopefully adding *this* doesn't cause double-milking.

--- data/eggmilksoup/powers/milk.sh
+++ data/eggmilksoup/powers/milk.sh
@@ -7,7 +7,7 @@ cat > milk.json <<EOF
 
 EOF
 
-for mob in glow_squid fox donkey cod chicken cat horse bat axolotl salmon pufferfish rabbit polar_bear pig piglin ocelot mule parrot wandering_trader sheep skeleton_horse villager snow_golem squid strider tropical_fish turtle goat enderman llama cave_spider bee panda zombified_piglin spider wolf dolphin trader_llama zoglin wither_skeleton witch blaze drowned creeper endermite evoker ghast guardian hoglin husk phantom piglin_brute magma_cube ravager shulker silverfish skeleton vindicator vex zombie
+for mob in glow_squid fox donkey cod chicken cat horse bat axolotl salmon pufferfish rabbit polar_bear pig piglin ocelot mule parrot wandering_trader sheep skeleton_horse villager snow_golem squid strider tropical_fish turtle enderman llama cave_spider bee panda zombified_piglin spider wolf dolphin trader_llama zoglin wither_skeleton witch blaze drowned creeper endermite evoker ghast guardian hoglin husk phantom piglin_brute magma_cube ravager shulker silverfish skeleton vindicator vex zombie stray
 do
 	cat >> milk.json <<EOF
 	"${mob}":
@@ -44,7 +44,7 @@ do
 EOF
 done
 cat >> milk.json <<EOF
-	"stray":
+	"goat":
 	{
 		"type": "origins:action_on_entity_use",
 		"bientity_condition":
@@ -53,7 +53,7 @@ cat >> milk.json <<EOF
 			"condition":
 			{
 				"type": "origins:entity_type",
-				"entity_type": "minecraft:stray"
+				"entity_type": "minecraft:goat"
 			}
 		},
 		"item_condition":
@@ -70,7 +70,38 @@ cat >> milk.json <<EOF
 		},
 		"result_stack":
 		{
-			"item": "minecraft:stray_milk",
+			"item": "minecraft:milk_bucket",
+			"amount": 1
+		}
+	},
+
+	"cow":
+	{
+		"type": "origins:action_on_entity_use",
+		"bientity_condition":
+		{
+			"type": "origins:target_condition",
+			"condition":
+			{
+				"type": "origins:entity_type",
+				"entity_type": "minecraft:cow"
+			}
+		},
+		"item_condition":
+		{
+			"type": "origins:ingredient",
+			"ingredient": {
+				"item": "minecraft:bucket"
+			}
+		},
+		"held_item_action":
+		{
+			"type": "origins:consume",
+			"amount": 1
+		},
+		"result_stack":
+		{
+			"item": "minecraft:milk_bucket",
 			"amount": 1
 		}
 	}
